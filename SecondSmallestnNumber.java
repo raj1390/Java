3d80f194fe213f92520baa4eef4de60d99cf8c04
raj1390 <116779631+raj1390@users.noreply.github.com>
2022-10-31 08:54:33 +0530

    Add files via upload
    Hi Dileep,
    Day 3
    Find my assignment program

---
+++ SecondSmallestnNumber.java
@@ -0,0 +1,16 @@
+package week1.day3;
+
+import java.util.Arrays;
+
+public class SecondSmallestnNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[] numbers = {23,45,67,32,89,22};
+		Arrays.sort(numbers);
+		System.out.println(numbers[1]);
+
+	}
+
+}
